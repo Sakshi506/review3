e5665ea2d13065e7f6d3cf7328bc2f8eb7ab08ca
Sakshi506 <70681398+Sakshi506@users.noreply.github.com>
2020-10-01 19:35:21 +0530

    final one

--- p1.sh
+++ p1.sh
@@ -2,9 +2,15 @@
 declare -A assDice_array
 assDice_array=([1]=0 [2]=0 [3]=0 [4]=0 [5]=0 [6]=0);
 count=1;
-sum=0;
-while [ count -le 10 ]
+while [ $count -le 10 ]
 do
 value=$((RANDOM%6+1))
 ((assDice_array[$value]++))
+count=$(($count+1))
 done
+
+echo "**************************************"
+for key in "${!assDice_array[@]}"
+do echo "$key comes => ${assDice_array[$key]}";  done
+echo "***************************************"
+
